bdbb85bafa4853ae3e5a71f88725ffafe6780974
opcionpro <69369061+opcionpro@users.noreply.github.com>
2020-08-20 19:56:17 -0400

    Version 1
    Primer version desde la plantilla

--- js/main.js
+++ js/main.js
@@ -26,6 +26,24 @@
         });
     });
 
+    /*------------------
+        Back to top button
+    --------------------*/
+
+    $(window).scroll(function () {
+        // console.log('wwww');
+        if ($(this).scrollTop() > 400) {
+            $('.js-top-scrollerx').fadeIn();
+        } else {
+            $('.js-top-scrollerx').fadeOut();
+        }
+    });
+    //Click event to scroll to top
+    $('.js-top-scrollerx').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1000);
+        return false;
+    });
+
 
     /*------------------
         Background Set
@@ -87,7 +105,7 @@
             }
         }
     });
-    
+
     /*------------------
 		Magnific
 	--------------------*/
@@ -110,5 +128,58 @@
     $(".datepicker").datepicker({
         minDate: 0
     });
+
+
+    /* Whatsapp Chat Widget by www.bloggermix.com */
+
+    $(document).on("click", "#send-it", function () {
+        var a = document.getElementById("chat-input");
+        if ("" != a.value) {
+            var b = "+59178186000",
+                c2 = document.getElementById("nombre").value,
+                c = document.getElementById("chat-input").value,
+                d = "https://web.whatsapp.com/send",
+                e = b,
+                f = "&text=" + c2 + ":%0A" + c;
+            if (
+                /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+                    navigator.userAgent
+                )
+            )
+                var d = "whatsapp://send";
+            var g = d + "?phone=" + e + f;
+            window.open(g, "_blank");
+        }
+    }),
+        $(document).on("click", ".informasi", function () {
+            (document.getElementById("get-number").innerHTML = $(this)
+                .children(".my-number")
+                .text()),
+                $(".start-chat,.get-new")
+                    .addClass("show")
+                    .removeClass("hide"),
+                $(".home-chat,.head-home")
+                    .addClass("hide")
+                    .removeClass("show"),
+                (document.getElementById("get-nama").innerHTML = $(this)
+                    .children(".info-chat")
+                    .children(".chat-nama")
+                    .text()),
+                (document.getElementById("get-label").innerHTML = $(this)
+                    .children(".info-chat")
+                    .children(".chat-label")
+                    .text());
+        }),
+        $(document).on("click", ".close-chat", function () {
+            $("#whatsapp-chat")
+                .addClass("hide")
+                .removeClass("show");
+        }),
+        $(document).on("click", ".blantershow-chat", function () {
+            $("#whatsapp-chat")
+                .addClass("show")
+                .removeClass("hide");
+        });
+
 
 })(jQuery);
